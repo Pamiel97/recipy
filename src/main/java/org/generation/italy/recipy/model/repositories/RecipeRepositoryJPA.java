cb6e7878c7734c1782c61a049ad3615f25645765
Pamela Milanese <milanesepamela@gmail.com>
2024-12-17 15:32:29 +0100

    interfaccia

--- src/main/java/org/generation/italy/recipy/model/repositories/RecipeRepositoryJPA.java
+++ src/main/java/org/generation/italy/recipy/model/repositories/RecipeRepositoryJPA.java
@@ -1,4 +1,7 @@
 package org.generation.italy.recipy.model.repositories;
 
-public interface RecipeRepositoryJPA {
+import org.generation.italy.recipy.model.entities.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecipeRepositoryJPA extends JpaRepository<Recipe, Long> {
 }
