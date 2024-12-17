dff9e3ea46af9b8174961d2398e438496059e882
Rolly64 <pietro.marinari@gmail.com>
2024-12-17 16:44:13 +0100

    correzione RecipesDto (category) e finita implementazione query recipesOkToUserDietType

---
+++ src/main/java/org/generation/italy/recipy/controllers/SuggestedRecipeController.java
@@ -0,0 +1,33 @@
+package org.generation.italy.recipy.controllers;
+
+import org.generation.italy.recipy.dtos.RecipeDto;
+import org.generation.italy.recipy.model.entities.Recipe;
+import org.generation.italy.recipy.model.services.abstraction.SuggestedRecipeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/home")
+public class SuggestedRecipeController {
+    private SuggestedRecipeService suggestedRecipeService;
+
+    public SuggestedRecipeController(SuggestedRecipeService suggestedRecipeService) {
+        this.suggestedRecipeService = suggestedRecipeService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<RecipeDto>> recipesOkToUserDietType(@PathVariable long userId){
+        List<RecipeDto> recipes = suggestedRecipeService.recipesOkToUserDietType(userId)
+                .stream().map(RecipeDto::fromRecipe).toList();
+        if (recipes.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(recipes);
+    }
+}
--- src/main/java/org/generation/italy/recipy/model/services/implementation/SuggestedRecipeServiceJpa.java
+++ src/main/java/org/generation/italy/recipy/model/services/implementation/SuggestedRecipeServiceJpa.java
@@ -1,6 +1,7 @@
 package org.generation.italy.recipy.model.services.implementation;
 
 import org.generation.italy.recipy.model.entities.Recipe;
+import org.generation.italy.recipy.model.repositories.SuggestedRecipeRepositoryJpa;
 import org.generation.italy.recipy.model.services.abstraction.SuggestedRecipeService;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +9,14 @@ import java.util.List;
 
 @Service
 public class SuggestedRecipeServiceJpa implements SuggestedRecipeService {
+    private SuggestedRecipeRepositoryJpa suggestedRecipeRepo;
+
+    public SuggestedRecipeServiceJpa(SuggestedRecipeRepositoryJpa suggestedRecipeRepositoryJpa) {
+        this.suggestedRecipeRepo = suggestedRecipeRepositoryJpa;
+    }
 
     @Override
     public List<Recipe> recipesOkToUserDietType(long userId) {
-        return List.of(); //TODO
+        return suggestedRecipeRepo.recipesOkToUserDietType(userId);
     }
 }
