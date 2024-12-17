e8c38b36fced1fa1daacba77622dd8b9d89d886a
Rolly64 <pietro.marinari@gmail.com>
2024-12-17 14:30:53 +0100

    aggiunge parametro profile in UserDto e inzia ReviewDto

--- src/main/java/org/generation/italy/recipy/dtos/ReviewDto.java
+++ src/main/java/org/generation/italy/recipy/dtos/ReviewDto.java
@@ -1,4 +1,18 @@
 package org.generation.italy.recipy.dtos;
 
-public class ReviewDto {
+import org.generation.italy.recipy.model.entities.Recipe; // da modificare
+import org.generation.italy.recipy.model.entities.Review;
+
+import java.time.format.DateTimeFormatter;
+
+public record ReviewDto(String text, double rating, String creationDate, UserDto userDto, Recipe recipe) {
+
+    public static ReviewDto fromReview(Review review){
+        return new ReviewDto(review.getText(),
+                review.getRating(),
+                review.getCreationDate().format(DateTimeFormatter.ISO_LOCAL_DATE),
+                UserDto.fromUser(review.getUser()),
+                review.getRecipe() // da modificare
+        );
+    }
 }
--- src/main/java/org/generation/italy/recipy/dtos/UserDto.java
+++ src/main/java/org/generation/italy/recipy/dtos/UserDto.java
@@ -3,16 +3,17 @@ package org.generation.italy.recipy.dtos;
 import org.generation.italy.recipy.model.entities.User;
 
 public class UserDto {
-    private String firstname, lastname;
+    private String firstname, lastname, profile;
 
     public UserDto() {}
-    public UserDto(String firstname, String lastname) {
+    public UserDto(String firstname, String lastname, String profile) {
         this.firstname = firstname;
         this.lastname = lastname;
+        this.profile = profile;
     }
 
     public static UserDto fromUser(User user) {
-        return new UserDto(user.getFirstname(), user.getLastname());
+        return new UserDto(user.getFirstname(), user.getLastname(), user.getProfile().toString());
     }
 
     public String getFirstname() {
@@ -26,5 +27,11 @@ public class UserDto {
     }
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+    public String getProfile() {
+        return profile;
+    }
+    public void setProfile(String profile) {
+        this.profile = profile;
     }
 }
