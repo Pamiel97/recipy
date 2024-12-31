cccce77a305a0fb1e74b6d3c3eb2cb41632b8922
Pamela Milanese <milanesepamela@gmail.com>
2024-12-31 12:09:23 +0100

    aggiunge get ingredienti

--- src/main/java/org/generation/italy/recipy/controllers/IngredientController.java
+++ src/main/java/org/generation/italy/recipy/controllers/IngredientController.java
@@ -5,13 +5,12 @@ import org.generation.italy.recipy.dtos.RecipeDto;
 import org.generation.italy.recipy.model.entities.Ingredient;
 import org.generation.italy.recipy.model.entities.Recipe;
 import org.generation.italy.recipy.model.services.abstraction.IngredientService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/ingredients")
@@ -28,6 +27,19 @@ public class IngredientController {
     public ResponseEntity<List<IngredientDto>> getAllIngredients(){
         List<Ingredient> allIngredient = ingredientService.findAllIngredient();
         return ResponseEntity.ok(allIngredient.stream().map(IngredientDto::fromIngredient).toList());
+    }
+
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/{id}")
+    public ResponseEntity<IngredientDto> getIngredientById(@PathVariable Long id) {
+        Optional<Ingredient> ingredientOptional = ingredientService.findById(id);
+        if (ingredientOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        Ingredient ingredient = ingredientOptional.get();
+        IngredientDto ingredientDto = IngredientDto.fromIngredient(ingredient);
+        return ResponseEntity.ok(ingredientDto);
     }
 
 }
