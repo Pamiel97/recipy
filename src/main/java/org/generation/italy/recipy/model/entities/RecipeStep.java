873ff6fec4cbaea0e797cb2e200c1ac99427d5b0
Rolly64 <pietro.marinari@gmail.com>
2025-01-07 17:42:03 +0100

    minor changes

--- src/main/java/org/generation/italy/recipy/model/entities/RecipeStep.java
+++ src/main/java/org/generation/italy/recipy/model/entities/RecipeStep.java
@@ -34,47 +34,36 @@ public class RecipeStep {
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public Recipe getRecipe() {
         return recipe;
     }
-
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
     }
-
     public Ingredient getIngredient() {
         return ingredient;
     }
-
     public void setIngredient(Ingredient ingredient) {
         this.ingredient = ingredient;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public int getOrdinal() {
         return ordinal;
     }
-
     public void setOrdinal(int ordinal) {
         this.ordinal = ordinal;
     }
-
     public String getStepImgUrl() {
         return stepImgUrl;
     }
-
     public void setStepImgUrl(String stepImgUrl) {
         this.stepImgUrl = stepImgUrl;
     }
