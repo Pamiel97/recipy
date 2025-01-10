9b8246b7d6f11207c87446ca135b63588e53bc65
Mirko Risuleo <mirkotm705@gmail.com>
2025-01-10 10:01:59 +0100

    Aggiunge metodo per prendere gli ingredienti impaginati e migliora la get degli ingredienti sortandoli in ordine alfabetico

--- src/main/java/org/generation/italy/recipy/controllers/IngredientController.java
+++ src/main/java/org/generation/italy/recipy/controllers/IngredientController.java
@@ -5,12 +5,17 @@ import org.generation.italy.recipy.dtos.RecipeDto;
 import org.generation.italy.recipy.model.entities.Ingredient;
 import org.generation.italy.recipy.model.entities.Recipe;
 import org.generation.italy.recipy.model.services.abstraction.IngredientService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/ingredients")
@@ -25,7 +30,7 @@ public class IngredientController {
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public ResponseEntity<List<IngredientDto>> getAllIngredients(){
-        List<Ingredient> allIngredient = ingredientService.findAllIngredient();
+        List<Ingredient> allIngredient = ingredientService.findAllIngredient().stream().sorted(Comparator.comparing(i -> i.getName())).toList();
         return ResponseEntity.ok(allIngredient.stream().map(IngredientDto::fromIngredient).toList());
     }
 
@@ -42,4 +47,11 @@ public class IngredientController {
         return ResponseEntity.ok(ingredientDto);
     }
 
+    @CrossOrigin
+    @GetMapping("/all")
+    public ResponseEntity<Page<IngredientDto>> getPaginatedIngredients(@RequestParam (defaultValue = "1") int page,
+                                                                       @RequestParam (defaultValue = "8") int size) {
+        Page<Ingredient> allIngredients = ingredientService.findAllIngredientImpaginated(page, size);
+        return ResponseEntity.ok(allIngredients.map(IngredientDto::fromIngredient));
+    }
 }
--- src/main/java/org/generation/italy/recipy/model/services/abstraction/IngredientService.java
+++ src/main/java/org/generation/italy/recipy/model/services/abstraction/IngredientService.java
@@ -3,6 +3,8 @@ package org.generation.italy.recipy.model.services.abstraction;
 import org.generation.italy.recipy.model.entities.Allergy;
 import org.generation.italy.recipy.model.entities.Ingredient;
 import org.generation.italy.recipy.model.entities.Intolerance;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +15,5 @@ public interface IngredientService {
     Optional<Allergy> findAllergyById(long id);
     Optional<Intolerance> findIntoleranceById(long id);
     List<Ingredient> findAllIngredient();
-
+    Page<Ingredient> findAllIngredientImpaginated(int page, int size);
 }
